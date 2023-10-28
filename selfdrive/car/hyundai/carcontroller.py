5d8a441c4568b9ac20c201c23762b886142c1877
multikyd <72329880+multikyd@users.noreply.github.com>
2023-10-28 21:52:42 +0900

    test

--- selfdrive/car/hyundai/carcontroller.py
+++ selfdrive/car/hyundai/carcontroller.py
@@ -412,13 +412,14 @@ class CarController:
       addr, bus = 0x7d0, 0
       self.radarDisableOverlapTimer += 1
       if self.radarDisableOverlapTimer >= 30:
-        if self.radarDisableOverlapTimer > 36:
+        if 200 >self.radarDisableOverlapTimer > 36:
           if self.frame % 41 == 0 or self.radarDisableOverlapTimer == 37:
             can_sends.append([addr, 0, b"\x02\x10\x03\x00\x00\x00\x00\x00", bus])
           elif self.frame % 43 == 0 or self.radarDisableOverlapTimer == 37:
             can_sends.append([addr, 0, b"\x03\x28\x03\x01\x00\x00\x00\x00", bus])
           elif self.frame % 19 == 0 or self.radarDisableOverlapTimer == 37:
             self.counter_init = True
+            print("OP Long Enabled")
             can_sends.append([addr, 0, b"\x02\x10\x85\x00\x00\x00\x00\x00", bus])  # radar disable
       else:
         self.counter_init = False
@@ -555,18 +556,18 @@ class CarController:
                                                 torque_fault, CS.lkas11, sys_warning, sys_state, CC.enabled,
                                                 hud_control.leftLaneVisible, hud_control.rightLaneVisible,
                                                 left_lane_warning, right_lane_warning, 0, self.ldws_fix, self.CP))
-      if self.CP.sccBus: # send lkas11 bus 1 or 2 if scc bus is
-        can_sends.append(hyundaican.create_lkas11(self.packer, self.frame, self.car_fingerprint, apply_steer, lat_active and not self.lkas_temp_disabled,
-                                                torque_fault, CS.lkas11, sys_warning, sys_state, CC.enabled,
-                                                hud_control.leftLaneVisible, hud_control.rightLaneVisible,
-                                                left_lane_warning, right_lane_warning, self.CP.sccBus, self.ldws_fix, self.CP))
-      if self.CP.mdpsBus: # send lkas11 bus 1 if mdps is bus 1
-        can_sends.append(hyundaican.create_lkas11(self.packer, self.frame, self.car_fingerprint, apply_steer, lat_active and not self.lkas_temp_disabled,
-                                                torque_fault, CS.lkas11, sys_warning, sys_state, CC.enabled,
-                                                hud_control.leftLaneVisible, hud_control.rightLaneVisible,
-                                                left_lane_warning, right_lane_warning, 1, self.ldws_fix, self.CP))
-        if self.frame % 2: # send clu11 to mdps if it is not on bus 0
-          can_sends.append(hyundaican.create_clu11(self.packer, self.frame, CS.clu11, Buttons.NONE, enabled_speed, self.CP.mdpsBus))
+      # if self.CP.sccBus: # send lkas11 bus 1 or 2 if scc bus is
+      #   can_sends.append(hyundaican.create_lkas11(self.packer, self.frame, self.car_fingerprint, apply_steer, lat_active and not self.lkas_temp_disabled,
+      #                                           torque_fault, CS.lkas11, sys_warning, sys_state, CC.enabled,
+      #                                           hud_control.leftLaneVisible, hud_control.rightLaneVisible,
+      #                                           left_lane_warning, right_lane_warning, self.CP.sccBus, self.ldws_fix, self.CP))
+      # if self.CP.mdpsBus: # send lkas11 bus 1 if mdps is bus 1
+      #   can_sends.append(hyundaican.create_lkas11(self.packer, self.frame, self.car_fingerprint, apply_steer, lat_active and not self.lkas_temp_disabled,
+      #                                           torque_fault, CS.lkas11, sys_warning, sys_state, CC.enabled,
+      #                                           hud_control.leftLaneVisible, hud_control.rightLaneVisible,
+      #                                           left_lane_warning, right_lane_warning, 1, self.ldws_fix, self.CP))
+      #   if self.frame % 2: # send clu11 to mdps if it is not on bus 0
+      #     can_sends.append(hyundaican.create_clu11(self.packer, self.frame, CS.clu11, Buttons.NONE, enabled_speed, self.CP.mdpsBus))
 
       if CS.out.cruiseState.standstill:
         self.standstill_status = 1
